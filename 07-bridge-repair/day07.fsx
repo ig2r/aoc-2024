402d751de6992f3a0d62338314215033561a9a25
Stephan Deininghaus <stephan@deininghaus.com>
2024-12-07 21:25:01 +0000

    Adapt Dec 7 solution for part 2

--- 07-bridge-repair/day07.fsx
+++ 07-bridge-repair/day07.fsx
@@ -17,19 +17,36 @@ let parseLine (line: string) =
       |> Array.map Int64.Parse
       |> Array.toList }
 
-let canEquationBeValid (equation: Equation) =
+let canEquationBeValid (operators: list<int64 -> int64 -> int64>) (equation: Equation) =
   let rec check intermediateResult remainingInput =
     match remainingInput with
     | [] -> intermediateResult = equation.Result
     | x :: xs ->
       if intermediateResult > equation.Result
         then false
-        else check (intermediateResult + x) xs || check (intermediateResult * x) xs
+        else List.exists (fun op -> check (op intermediateResult x) xs) operators
   check 0 equation.Inputs
 
 let equations = Array.map parseLine input
 
-// Solution for part 1:
-let validEquations = Array.filter canEquationBeValid equations
-let totalCalibrationResult = Array.fold (fun sum e -> sum + e.Result) 0L validEquations
-printfn "totalCalibrationResult: %d" totalCalibrationResult
+// Solution to part 1, which permits only left-to-right addition and multiplication operators.
+let solvePart1 =
+  let operators = [ (+); (*) ]
+  equations
+    |> Array.filter (canEquationBeValid operators)
+    |> Array.fold (fun sum e -> sum + e.Result) 0L
+
+// Defines a concatenation operator '||' such that 15 || 6 = 156.
+let concatDigits (x: int64) (y: int64) =
+  let shiftFactor = int(Math.Log10(double(y))) + 1
+  x * int64(Math.Pow(10, shiftFactor)) + y
+
+// Solution to part 2, which includes the special '||' concatenation operator as well.
+let solvePart2 =
+  let operators = [ (+); (*); concatDigits ]
+  equations
+    |> Array.filter (canEquationBeValid operators)
+    |> Array.fold (fun sum e -> sum + e.Result) 0L
+
+printfn "Total calibration result (part 1): %d" solvePart1
+printfn "Total calibration result (part 2): %d" solvePart2
