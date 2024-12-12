7bf6dc7bf9c2d2236ef66923987632f14e2534da
Stephan Deininghaus <stephan@deininghaus.com>
2024-12-12 22:45:29 +0000

    Simplify Dec 11 solution

--- 11-plutonian-pebbles/day11.exs
+++ 11-plutonian-pebbles/day11.exs
@@ -4,7 +4,7 @@ pebble_counts =
   data
   |> String.trim()
   |> String.split(" ")
-  |> Map.new(fn p -> {p, 1} end)
+  |> Map.from_keys(1)
 
 transmogrify = fn
   "0" ->
@@ -14,31 +14,25 @@ transmogrify = fn
     l = String.length(pebble)
 
     if rem(l, 2) == 0 do
-      mid = div(l, 2)
-      p1 = String.slice(pebble, 0, mid)
-      p2 = String.slice(pebble, mid, mid) |> String.to_integer() |> Integer.to_string()
-      [p1, p2]
+      {p1, p2} = String.split_at(pebble, div(l, 2))
+      p2_stripped_zeroes = p2 |> String.to_integer() |> Integer.to_string()
+      [p1, p2_stripped_zeroes]
     else
       [(2024 * String.to_integer(pebble)) |> Integer.to_string()]
     end
 end
 
-blink_once = fn pebble_counts ->
+blink_once = fn pebble_counts when is_map(pebble_counts) ->
   pebble_counts
-  |> Enum.flat_map(fn {p, c} ->
-    ts = transmogrify.(p)
-    Enum.map(ts, fn t -> {t, c} end)
-  end)
-  |> Enum.reduce(
-    Map.new(),
-    fn {t, c}, acc -> Map.update(acc, t, c, fn v -> v + c end) end)
+  |> Enum.flat_map(fn {p, c} -> Enum.map(transmogrify.(p), fn t -> {t, c} end) end)
+  |> Enum.reduce(Map.new(), fn {t, c}, acc -> Map.update(acc, t, c, fn v -> v + c end) end)
 end
 
-blink_times = fn pebble_counts, n ->
+blink_times = fn pebble_counts, n when is_map(pebble_counts) and is_integer(n) ->
   Enum.reduce(1..n, pebble_counts, fn _, acc -> blink_once.(acc) end)
 end
 
-sum_pebbles = fn pebble_counts ->
+sum_pebbles = fn pebble_counts when is_map(pebble_counts) ->
   pebble_counts |> Enum.reduce(0, fn {_, c}, acc -> acc + c end)
 end
 
