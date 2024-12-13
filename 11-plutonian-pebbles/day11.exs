13ba675de66e75847666a698267944673e085b0f
Stephan Deininghaus <stephan@deininghaus.com>
2024-12-13 20:33:38 +0000

    Further simplify Dec 11 solution

--- 11-plutonian-pebbles/day11.exs
+++ 11-plutonian-pebbles/day11.exs
@@ -1,43 +1,44 @@
+require Integer
+
 {:ok, data} = File.read("input.txt")
 
 pebble_counts =
   data
-  |> String.trim()
-  |> String.split(" ")
+  |> String.split()
+  |> Enum.map(&String.to_integer/1)
   |> Map.from_keys(1)
 
 transmogrify = fn
-  "0" ->
-    ["1"]
+  0 ->
+    [1]
 
   pebble ->
-    l = String.length(pebble)
+    digits = Integer.digits(pebble)
 
-    if rem(l, 2) == 0 do
-      {p1, p2} = String.split_at(pebble, div(l, 2))
-      p2_stripped_zeroes = p2 |> String.to_integer() |> Integer.to_string()
-      [p1, p2_stripped_zeroes]
+    if Integer.is_even(length(digits)) do
+      {p1, p2} = Enum.split(digits, div(length(digits), 2))
+      [p1, p2] |> Enum.map(&Integer.undigits/1)
     else
-      [(2024 * String.to_integer(pebble)) |> Integer.to_string()]
+      [2024 * pebble]
     end
 end
 
-blink_once = fn pebble_counts when is_map(pebble_counts) ->
+blink_once = fn pebble_counts ->
   pebble_counts
   |> Enum.flat_map(fn {p, c} -> Enum.map(transmogrify.(p), fn t -> {t, c} end) end)
-  |> Enum.reduce(Map.new(), fn {t, c}, acc -> Map.update(acc, t, c, fn v -> v + c end) end)
+  |> Enum.reduce(%{}, fn {t, c}, acc -> Map.update(acc, t, c, fn v -> v + c end) end)
 end
 
-blink_times = fn pebble_counts, n when is_map(pebble_counts) and is_integer(n) ->
+blink_times = fn pebble_counts, n ->
   Enum.reduce(1..n, pebble_counts, fn _, acc -> blink_once.(acc) end)
 end
 
-sum_pebbles = fn pebble_counts when is_map(pebble_counts) ->
-  pebble_counts |> Enum.reduce(0, fn {_, c}, acc -> acc + c end)
+sum_pebbles = fn pebble_counts ->
+  pebble_counts |> Map.values() |> Enum.sum()
 end
 
-result_part1 = sum_pebbles.(blink_times.(pebble_counts, 25))
-result_part2 = sum_pebbles.(blink_times.(pebble_counts, 75))
+result_part1 = pebble_counts |> blink_times.(25) |> sum_pebbles.()
+result_part2 = pebble_counts |> blink_times.(75) |> sum_pebbles.()
 
 IO.puts("Part 1: #{result_part1}")
-IO.puts("Part 1: #{result_part2}")
+IO.puts("Part 2: #{result_part2}")
